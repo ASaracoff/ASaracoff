b56b9f025887db1c20e0f0e198e82461819f2c17
ASaracoff <alfapuppy@gmail.com>
2025-08-31 20:15:40 -0600

    added Square Cards to p1

---
+++ squareCards.js
@@ -0,0 +1,27 @@
+// source: https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=19
+
+// only thing changed is the [] index #
+document.getElementsByClassName("article")[0].addEventListener("click",function(){
+    window.open("proj/film2018/index.html") //where do you want to send them to?
+}
+)
+
+document.getElementsByClassName("article")[1].addEventListener("click",function(){
+    window.open("proj/movies2012/index.html") //where do you want to send them to?
+}
+)
+
+document.getElementsByClassName("article")[2].addEventListener("click",function(){
+    window.open("JS/sandbox/sideProjects/calculator/index.html") //where do you want to send them to?
+}
+)
+
+document.getElementsByClassName("article")[3].addEventListener("click",function(){
+    window.open("JS/sandbox/sideProjects/tictactoe/tictactoe.html") //where do you want to send them to?
+}
+)
+document.getElementsByClassName("article")[4].addEventListener("click",function(){
+    window.open("JS/sandbox/sideProjects/squareCards/squareCards.html") //where do you want to send them to?
+}
+)
+
