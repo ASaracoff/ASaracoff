cba6eb5bba07ba2bdabe22d2667216a0b3ac45b7
ASaracoff <alfapuppy@gmail.com>
2025-09-07 16:33:15 -0600

    ceasar2

--- JS/ceasarCipher/caesar2.js
+++ JS/ceasarCipher/caesar2.js
@@ -4,13 +4,57 @@ const friend = 'BRUTUS';
 const shiftValue = 3;
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-function encryptLetter(letter){
-    
-    if (!alphabet.includes(letter.toLowerCase())){
-            console.log('Error');
-        }else{
-    
-        let newLetter = (letter + shiftValue) % alphabet.  length;
-        }
-    return newLetter
+
+// Step 1 
+// encrypt a letter
+function encryptLetter(letter, shift) {
+    const index = alphabet.indexOf(letter.toLowerCase());
+    const newIndex = (index + shift) % alphabet.length; //current letter index + shift number
+        return alphabet[newIndex]    
 }
+
+console.log(encryptLetter('a',3))
+
+
+// Step 2
+// encypt a whole message
+function encryptMessage(word, shift){
+    let encryptedMessage = ''
+    for( i=0 ; i < word.length; i++){
+        encryptedMessage += encryptLetter(word[i], shift);
+    }
+    return encryptedMessage;
+}
+
+console.log(encryptMessage('BRUTUS',3))
+
+
+// Step 3 
+// decrypt letter
+function decryptLetter(letter, shift) {
+    const index = alphabet.indexOf(letter.toLowerCase());
+    const newIndex = (index - shift) % alphabet.length; //current letter index + shift number
+        return alphabet[newIndex]    
+}
+
+console.log(decryptLetter('d',3))
+
+// Step 4
+// decrypt message
+// Step 2
+// encypt a whole message
+function decryptMessage(word, shift){
+    let decryptedMessage = ''
+    for( i=0 ; i < word.length; i++){
+        decryptedMessage += decryptLetter(word[i], shift);
+    }
+    return decryptedMessage.toUpperCase();
+}
+
+console.log(decryptMessage('euxwxv',3))
+
+
+// Question:
+// encrypt 'BRUTUS' then decrypts it, will he receive 'BRUTUS' back? Why/Why not?
+
+// as it is atm yes and no. yes it will say brutus, but in lowercase letters. can be modified to return in all capitalized characters if you want. simply return 'decryptedMessage.toUpperCase()' 
