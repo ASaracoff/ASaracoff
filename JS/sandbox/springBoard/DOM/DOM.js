505326142422300c2f3f4df37f320fdff2aadbd4
ASaracoff <alfapuppy@gmail.com>
2025-10-24 09:38:35 -0600

    updated org

---
+++ JS/sandbox/springBoard/DOM/DOM.js
@@ -0,0 +1,26 @@
+// source: https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=19
+
+document.getElementsByClassName("article")[0].addEventListener("click",function(){
+    window.open("MemeGenerator/memeGenerator.html")
+}
+)
+
+document.getElementsByClassName("article")[1].addEventListener("click",function(){
+    window.open("DOMmanipulation/index.html")
+}
+)
+
+document.getElementsByClassName("article")[2].addEventListener("click",function(){
+    window.open("logoMaker/logoMaker.html")
+}
+)
+
+document.getElementsByClassName("article")[3].addEventListener("click",function(){
+    window.open("mouseMap/mouseMap.html")
+}
+)
+
+document.getElementsByClassName("article")[4].addEventListener("click",function(){
+    window.open("randomColors/randomColors.html")
+}
+)
