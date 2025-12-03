edbffe3e98f2494690f0611c51844e3e4a34185f
ASaracoff <alfapuppy@gmail.com>
2025-12-03 09:41:13 -0700

    style.css + :root colors
    made it so that if I choose to change the color scheme of the home page it will be fast and smooth

--- JS/Notes/APIs/2 APIs/APIs.js
+++ JS/Notes/APIs/2 APIs/APIs.js
@@ -1,3 +1,17 @@
+// APIs that work:
+    // https://dog.ceo/api/breeds/image/random
+
+    // https://reqres.in
+        // https://reqres.in/api/users
+        // need api key
+            // not really sure how to insert into code along with the teach
+            // curl -X GET "https://reqres.in/api/users" 
+            // -H "Content-Type: application/json" 
+            // -H "x-api-key: {{reqres_40adedd210ee40f6bb43ce93d66ddbd0}}"
+        // free sign up
+
+
+
 // APIs
 // Goals
     // define api
@@ -22,3 +36,139 @@
 // What is an API?
     // set of clearly defined methods of communication btwn various components
     // an API may be for a web-base system, OS, DB system, computer hardware, or software library
+
+// AJAX  Axios
+    // https://axios-http.com
+    // simple promise based HTTP client for browser and node.js
+    // provides simple to use library in a small package with a very extensible interface
+
+// You don't have to use Axios for this
+    // old clunky built-in tool: XMLHttpRequest
+    // or newer still clunky 'fetch'
+    // or lots of other libraries like jQuery
+    // BUT.... axios for now! featureful and popular!
+        // axios built ontop of (?) fetch
+
+// Getting Axios
+    // easily included using a CDN link:
+        // <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        // <script src="mycode.js" defer></script>
+        
+    // download via gitHub or axios getting started pg
+    // insert in html page before the link to your own JS code
+
+// Make a Simple Request
+    // axios.get(url)
+    // Make a GET request to that URL
+    // const request = axios.get('https://swapi.dev/api/planets/')
+        // PROBLEM!  i have been blocked, "blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource."
+    // let card = axios.get("/api/card");
+    // console.log(card)
+
+// What is a promise?
+    // more detail to come when we get to Node
+    // all we need to know for now: 
+        // a promise is like a placeholder for a future value
+        // we need to WAIT for a value or a potential error...
+
+    // JS is handing the browser the requests until we get a value/error
+    // we WANT to wait for the promise to have that value before proceeding
+    // but we don't knowwhen the promis will receive its value
+
+// Asynchronicity
+    // AJAX requests are asynchronous
+    // async -> declare a function to wait for the api
+    // await -> wait until we get something back
+    async function getData() {
+        const request = await axios.get('https://swapi.dev/api/planets/')
+        console.log(response)
+        for(let planet of response.data.results) {
+            console.log(planet.name)
+        }
+    }
+
+    // works! 
+        // const res = await axios.get('https://dog.ceo/api/breeds/image/random')
+        
+
+    async function getRandomDog() {
+    const res = await axios.get('https://dog.ceo/api/breeds/image/random')
+    console.log(res.data)   
+    const img = document.querySelector('#dog')
+    img.src = res.data.message
+    }
+
+    getRandomDog()
+
+    async function getDogByBreed(breed) {
+        try{
+            const url = (`https://dog.ceo/api/breed/${breed}/images/random`)
+        const res = await axios.get(url)
+        const img = document.querySelector('#dog')
+        img.src = res.data.message
+        } catch(e) {
+            getRandomDog()
+            alert("BREED NOT FOUND: heres a random dog! :)")
+        }
+    }
+    
+// what to do when throwing errors?
+    // try and catch
+    try{
+        // run your code
+    }catch{
+        // run this if there's a problem
+    }
+
+const form = document.querySelector('#searchform')
+const input = document.querySelector('#search')
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    console.log("submit")
+    console.log(input.value)
+    getDogByBreed(input.value)
+    input.value = "" //reset value
+})
+
+
+// Axios API
+
+// .get
+    // axios.get(url, [config])
+    // config - optional object many Axios methods use 
+    // it holds specific configurations for what you need
+
+// .get w Query Params
+    // to request for /resource?a=1&b=2 :
+        // axios.get("/resource?a=1&b=2") OR
+        // axios.get("/resource", {params: {a:1, b:2}})
+
+    // exists but pops up as a 'roal togel' slot game O.o
+    // same block - CORS policy no 'access-control-allow-origin'
+    // http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe
+
+    async function getJoke(first,last) {
+        let res = await axios.get(`http://api.icndb.com/jokes/random?firstName=${first}&amp;lastName=${last}`)
+        console.log(res/data.value.joke)
+    }
+
+    async function getJokeShort(firstName,lastName) {
+        let res = await axios.get(`http://api.icndb.com/jokes/random`, { params: {firstName, lastName}})
+        console.log(res/data.value.joke)
+    }
+
+// .post
+    // axios.post(url, [data], [config])
+    // similar to axios.get BUT uses a POST request
+
+    // https://reqres.in
+    // 'https://reqres.in/api/users'
+    async function getUsers() {
+        const res = await axios.get('https://reqres.in/api/users')
+        console.log(res)
+    }
+
+    async function createUser() {
+        const res = await axios.pos('https://reqres.in/api/users', {username:'hi', email:'hi@gmail', age: '1'})
+        console.log(res)
+    }
---
+++ carousel.php
@@ -0,0 +1,85 @@
+<h4 class="carouselHeading">Website Archive</h4>
+    <!-- 7 -->
+    <div class="carousel">
+        <div class="group">
+            <div class="card"><a href="sites/sites/2018film/index.html" target="_blank"><img src="sites/img/s1.png" alt="film noir"></a></div>
+            <div class="card"><a href="sites/sites/2017webDesign/assign212.html" target="_blank"><img src="sites/img/s2.png" alt="web design course"></a></div>
+            <div class="card"><a href="sites/sites/2017teamTj/default.html" target="_blank"><img src="sites/img/s3.png" alt="team tj"></a></div>
+            <div class="card"><a href="sites/sites/2013trial/index.htm" target="_blank"><img src="sites/img/s4.png" alt="interface trials"></a></div>
+            <div class="card"><a href="sites/sites/2013fireworks/index.htm" target="_blank"><img src="sites/img/s5.png" alt="tea 4 you - fireworks"></a></div>
+            <div class="card"><a href="sites/sites/2011movies/index.html" target="_blank"><img src="sites/img/s6.png" alt="movie selection"></a></div>
+            <div class="card"><a href="sites/sites/2011dreamweaver/index.html" target="_blank"><img src="sites/img/s7.png" alt="theMMM"></a></div>
+        </div>
+        <div aria-hidden class="group">
+            <div class="card"><a href="sites/sites/2018film/index.html" target="_blank"><img src="sites/img/s1.png" alt="film noir"></a></div>
+            <div class="card"><a href="sites/sites/2017webDesign/assign212.html" target="_blank"><img src="sites/img/s2.png" alt="web design course"></a></div>
+            <div class="card"><a href="sites/sites/2017teamTj/default.html" target="_blank"><img src="sites/img/s3.png" alt="team tj"></a></div>
+            <div class="card"><a href="sites/sites/2013trial/index.htm" target="_blank"><img src="sites/img/s4.png" alt="interface trials"></a></div>
+            <div class="card"><a href="sites/sites/2013fireworks/index.htm" target="_blank"><img src="sites/img/s5.png" alt="tea 4 you - fireworks"></a></div>
+            <div class="card"><a href="sites/sites/2011movies/index.html" target="_blank"><img src="sites/img/s6.png" alt="movie selection"></a></div>
+            <div class="card"><a href="sites/sites/2011dreamweaver/index.html" target="_blank"><img src="sites/img/s7.png" alt="theMMM"></a></div>
+        </div>
+    </div>
+
+<h4 class="carouselHeading">SpringBoard Projects</h4>
+    <!-- 6 -->
+    <div class="carousel">
+        <div class="group">
+            <div class="card"><a href="JS/sandbox/springBoard/MemeGenerator/memeGenerator.html" target="_blank"><img src="JS/sandbox/springBoard/img/sb1.png" alt="meme generator"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/DOMEvents/index.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM1.png" alt="DOM Events"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/DOMmanipulation/index.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM2.png" alt="DOM Manipulation"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/logoMaker/logoMaker.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM3.png" alt="logo maker"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/mouseMap/mouseMap.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM4.png" alt="mouse map"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/randomColors/randomColors.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM5.png" alt="Random Colors"></a></div>
+        </div>
+        <div aria-hidden class="group">
+            <!-- add duplicate of above when ready -->
+            <div class="card"><a href="JS/sandbox/springBoard/MemeGenerator/memeGenerator.html" target="_blank"><img src="JS/sandbox/springBoard/img/sb1.png" alt="meme generator"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/DOMEvents/index.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM1.png" alt="DOM Events"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/DOMmanipulation/index.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM2.png" alt="DOM Manipulation"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/logoMaker/logoMaker.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM3.png" alt="logo maker"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/mouseMap/mouseMap.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM4.png" alt="mouse map"></a></div>
+            <div class="card"><a href="JS/sandbox/springBoard/DOM/randomColors/randomColors.html" target="_blank"><img src="JS/sandbox/springBoard/DOM/img/DOM5.png" alt="Random Colors"></a></div>
+        </div>
+
+    </div>
+
+    <!-- side project -->
+    <h4 class="carouselHeading">Side Projects</h4>
+    <!-- 13 -->
+    <div class="carousel">
+        <div class="group">
+            <div class="card"><a href="JS/sandbox/sideProjects/calculator/index.html" target="_blank"><img src="img/sp/sp1.png" alt="Calculator"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/tictactoe/tictactoe.html" target="_blank"><img src="img/sp/sp2.png" alt="tic-tac-toe"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/squareCards/squareCards.html" target="_blank"><img src="img/sp/sp3.png" alt="Square Cards"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/5minis/combo.html" target="_blank"><img src="img/sp/sp4.png" alt="5 mini projects"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/snake/snake.html" target="_blank"><img src="img/sp/sp5.png" alt="snake game"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/pong/pong.html" target="_blank"><img src="img/sp/sp6.png" alt="pong game"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/7games/7games.html" target="_blank"><img src="img/sp/sp7.png" alt="7 classic games"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/stickyNotes/stickyNotes.html" target="_blank"><img src="img/sp/sp8.png" alt="sticky notes"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/todoLocalStorage/todoLS.html" target="_blank"><img src="img/sp/sp9.png" alt="Todo local storage"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/dragDrop/dragDrop.html" target="_blank"><img src="img/sp/sp10.png" alt="drag and drop"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/stickyNotesLS/stickyNotesLS.html" target="_blank"><img src="img/sp/sp11.png" alt="sticky notes local storage"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/quoteGen/quoteGen.html" target="_blank"><img src="img/sp/sp12.png" alt="api quote generator"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/infiniteCarousel/infiniteCarousel.html" target="_blank"><img src="img/sp/sp13.png" alt="Infinite Carousel"></a></div>
+            <!-- <div class="card"><a href="JS/sandbox/sideProjects/new/new.html" target="_blank"><img src="img/sp/sp14.png" alt="new"></a></div> -->
+        </div>
+        <!-- duplicate group: aria-hidden hides the visual(?) -->
+        <div aria-hidden class="group">
+            <!-- add duplicate of above when ready -->
+            <div class="card"><a href="JS/sandbox/sideProjects/calculator/index.html" target="_blank"><img src="img/sp/sp1.png" alt="Calculator"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/tictactoe/tictactoe.html" target="_blank"><img src="img/sp/sp2.png" alt="tic-tac-toe"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/squareCards/squareCards.html" target="_blank"><img src="img/sp/sp3.png" alt="Square Cards"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/5minis/combo.html" target="_blank"><img src="img/sp/sp4.png" alt="5 mini projects"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/snake/snake.html" target="_blank"><img src="img/sp/sp5.png" alt="snake game"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/pong/pong.html" target="_blank"><img src="img/sp/sp6.png" alt="pong game"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/7games/7games.html" target="_blank"><img src="img/sp/sp7.png" alt="7 classic games"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/stickyNotes/stickyNotes.html" target="_blank"><img src="img/sp/sp8.png" alt="sticky notes"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/todoLocalStorage/todoLS.html" target="_blank"><img src="img/sp/sp9.png" alt="Todo local storage"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/dragDrop/dragDrop.html" target="_blank"><img src="img/sp/sp10.png" alt="drag and drop"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/stickyNotesLS/stickyNotesLS.html" target="_blank"><img src="img/sp/sp11.png" alt="sticky notes local storage"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/quoteGen/quoteGen.html" target="_blank"><img src="img/sp/sp12.png" alt="api quote generator"></a></div>
+            <div class="card"><a href="JS/sandbox/sideProjects/infiniteCarousel/infiniteCarousel.html" target="_blank"><img src="img/sp/sp13.png" alt="Infinite Carousel"></a></div>
+            <!-- <div class="card"><a href="JS/sandbox/sideProjects/new/new.html" target="_blank"><img src="img/sp/sp14.png" alt="new"></a></div> -->
+        </div>
+    </div>
